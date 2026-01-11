a03bb64c263c5ce99f41c15dfcf7153c7884e228
Essa034 <essaann034@gmail.com>
2026-01-11 04:11:45 +0000

    shuffled about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,10 @@
 import "./About.css"
 
 const highlightCards = [
+  {title: "Concreate",
+    body:
+    "CONCREATE ’26, organized by ASCE MACE,ICI MACE and BAI is a dynamic two-day technical fest that blends innovation, creativity, and pure excitement. Taking place on 16th & 17th January, CONCREATE brings together curious minds and passionate builders for a series of thrilling challenges. From brainstorming bold ideas at the Ideathon and tackling real-world problems in the Sustainable Design Challenge, to racing through clues in the Treasure Hunt, testing teamwork in Bridge Making, and outsmarting opponents in Capture the Flag, there’s something for everyone. Whether you’re here to compete, collaborate, or simply enjoy the vibe, CONCREATE ’26 promises energy, learning, fun, and memories that last. Outthink. Outbuild. Outlast."
+  },
   {
     title: "ASCE",
     body:
@@ -17,10 +21,7 @@ const highlightCards = [
     body:
       "BAI (Builders’ Association of India) is an open platform for students from every stream who want to explore leadership, teamwork, and the professional side of large-scale projects. It focuses on collaboration, industry awareness, and skill development that are valuable across all fields. If you’re curious about how ideas turn into reality, BAI is the place for you.",
   },
-  {title: "Concreate",
-    body:
-    "CONCREATE ’26, organized by ASCE MACE,ICI MACE and BAI is a dynamic two-day technical fest that blends innovation, creativity, and pure excitement. Taking place on 16th & 17th January, CONCREATE brings together curious minds and passionate builders for a series of thrilling challenges. From brainstorming bold ideas at the Ideathon and tackling real-world problems in the Sustainable Design Challenge, to racing through clues in the Treasure Hunt, testing teamwork in Bridge Making, and outsmarting opponents in Capture the Flag, there’s something for everyone. Whether you’re here to compete, collaborate, or simply enjoy the vibe, CONCREATE ’26 promises energy, learning, fun, and memories that last. Outthink. Outbuild. Outlast."
-  }
+  
 ]
 
 function About() {
